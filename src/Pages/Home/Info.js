9d27c73c6617979a3b972da900925ef9ee2a178c
yaminhossen <streatdot616@gmail.com>
2022-05-11 21:33:00 +0600

    modal dynamic slot added

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -8,7 +8,7 @@ const Info = () => {
     return (
         <div className='grid grid-cols-1  lg:grid-cols-3 gap-5'>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Opening Hour" img={clock}></InfoCard>
-            <InfoCard bgClass="bg-neutral" cardTitle="Our Loctations" img={marker}></InfoCard>
+            <InfoCard bgClass="bg-[#3d4451]" cardTitle="Our Loctations" img={marker}></InfoCard>
             <InfoCard bgClass="bg-gradient-to-r from-secondary to-primary" cardTitle="Contact Us" img={phone}></InfoCard>
         </div>
     );
