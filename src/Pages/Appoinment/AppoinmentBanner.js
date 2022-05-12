1d19836193acc5816aa974e241f994155773585d
yaminhossen <streatdot616@gmail.com>
2022-05-12 11:15:07 +0600

    login component complite

--- src/Pages/Appoinment/AppoinmentBanner.js
+++ src/Pages/Appoinment/AppoinmentBanner.js
@@ -7,9 +7,9 @@ import { format } from 'date-fns';
 const AppoinmentBanner = ({ date, setDate }) => {
 
     return (
-        <div class="hero min-h-screen ">
-            <div class="hero-content flex-col lg:flex-row-reverse">
-                <img src={chair} class="max-w-sm ml-20 rounded-lg shadow-2xl" />
+        <div className="hero min-h-screen ">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <img src={chair} className="max-w-sm ml-20 rounded-lg shadow-2xl" />
                 <div>
                     <DayPicker
                         mode="single"
