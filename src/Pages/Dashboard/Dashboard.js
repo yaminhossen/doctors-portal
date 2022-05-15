25d99877162321ac469666e878e8a74ab4c07d40
yaminhossen <streatdot616@gmail.com>
2022-05-15 07:22:25 +0600

    module 74 closed

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -12,10 +12,10 @@ const Dashboard = () => {
             </div>
             <div class="drawer-side">
                 <label for="dashboard-sidebar" class="drawer-overlay"></label>
-                <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+                <ul class="menu p-4 overflow-y-auto w-50 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard'>Sidebar Item 1</Link></li>
-                    <li><Link to='/dashboard/review'>Sidebar Item 2</Link></li>
+                    <li><Link to='/dashboard'>My Appoinments</Link></li>
+                    <li><Link to='/dashboard/review'>My Reviews</Link></li>
                 </ul>
 
             </div>
--- src/Pages/Dashboard/MyAppoinment.js
+++ src/Pages/Dashboard/MyAppoinment.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const MyAppoinment = () => {
+    const [appoinments, setAppoinments] = useState([]);
+    const [user] = useAuthState(auth);
+
+    useEffect(() => {
+        if (user) {
+            fetch(`http://localhost:5000/booking?patient=${user.email}`)
+                .then(res => res.json())
+                .then(data => setAppoinments(data))
+        }
+    }, [user])
     return (
         <div>
-            <h3>my appoinment</h3>
+            <h3>My appoinment: {appoinments.length}</h3>
+            <div class="overflow-x-auto">
+                <table class="table w-full">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Date</th>
+                            <th>Time</th>
+                            <th>Treatment</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            appoinments.map((a, index) => <tr>
+                                <th>{index + 1}</th>
+                                <td>{a.patientName}</td>
+                                <td>{a.date}</td>
+                                <td>{a.slot}</td>
+                                <td>{a.treatment}</td>
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     );
 };
